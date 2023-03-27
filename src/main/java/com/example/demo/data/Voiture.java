b56802a992eac90e2e9472adc6498e39ca5a9b59
joebotseke <brandon.bots777@gmail.ccom>
2023-03-27 15:34:16 +0200

    main

---
+++ src/main/java/com/example/demo/data/Voiture.java
@@ -0,0 +1,46 @@
+package com.example.demo.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Voiture {
+    int id;
+    String marque;
+    int prix;
+
+    public Voiture(int id, String marque, int prix) {
+        this.id = id;
+        this.marque = marque;
+        this.prix = prix;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
